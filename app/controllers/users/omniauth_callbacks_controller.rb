5aeba74872f7284becbd6e2397f8b27c00fe999c
Sumit <sumit.raghuwanshi192@gmail.com>
2019-11-28 12:58:53 +0530

    Modify code

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,17 +1,17 @@
 class Users::OmniauthCallbacksController < ApplicationController
 	 def facebook
-        @user = UserProvider.find_for_facebook_oauth(request.env["omniauth.auth"])
+      @user = UserProvider.find_for_provider_oauth(request.env["omniauth.auth"])
 
-        if @user.persisted?
-          sign_in_and_redirect @user, :event => :authentication               
-        else
-          session["devise.facebook_data"] = request.env["omniauth.auth"]
-          redirect_to new_user_registration_url
-        end
+      if @user.persisted?
+        sign_in_and_redirect @user, :event => :authentication               
+      else
+        session["devise.facebook_data"] = request.env["omniauth.auth"]
+        redirect_to new_user_registration_url
+      end
     end
 
     def google_oauth2
-        @user = UserProvider.find_for_google_oauth(request.env["omniauth.auth"])
+        @user = UserProvider.find_for_provider_oauth(request.env["omniauth.auth"])
         if @user.persisted?
           sign_in_and_redirect @user, :event => :authentication
         else
--- app/models/user_provider.rb
+++ app/models/user_provider.rb
@@ -1,61 +1,111 @@
 class UserProvider < ApplicationRecord
   belongs_to :user
-def self.find_for_facebook_oauth(auth)
-    user = UserProvider.where(:provider => auth.provider, :uid => auth.uid).first
-    unless user.nil?
-        user.user
-    else
-        registered_user = User.where(:email => auth.info.email).first
-        unless registered_user.nil?
-                    UserProvider.create!(
-                          provider: auth.provider,
-                          uid: auth.uid,
-                          user_id: registered_user.id
-                          )
-            registered_user
-        else
-            user = User.create!(
-                name: auth.info.name,
-                        email: auth.info.email,
-                        password: Devise.friendly_token[0,20],
-                        )
-            user_provider = UserProvider.create!(
-                provider:auth.provider,
-                        uid:auth.uid,
-                          user_id: user.id
-                )
-            user
-        end
-    end
-end
+  
+  def self.find_for_provider_oauth(auth)
+    user_provider = UserProvider.where(:provider => auth.provider, :uid => auth.uid).first
 
-def self.find_for_google_oauth(auth)
-    user = UserProvider.where(:provider => auth.provider, :uid => auth.uid).first
-    unless user.nil?
-        user.user
+    if user_provider.present?
+      user_provider.user
+      # "User login and redirect to landing page"
     else
-        registered_user = User.where(:email => auth.info.email).first
-        unless registered_user.nil?
-                    UserProvider.create!(
-                          provider: auth.provider,
-                          uid: auth.uid,
-                          user_id: registered_user.id
-                          )
-            registered_user
-        else
-            user = User.create!(
-                name: auth.info.name,
-                          email: auth.info.email,
-                          password: Devise.friendly_token[0,20],
-                        )
-            user_provider = UserProvider.create!(
-                provider:auth.provider,
-                        uid:auth.uid,
-                        user_id: user.id
-                )
-            user
-        end
+      # New user signup
+      # Email: abc@gmail.com
+      # FB: abc@gmail.com
+      user = User.where(email: auth.info.email).first
+      # "fetch email from auth details and if Email ID already exist"
+      if user.present?
+        user_provider = user.user_providers.new(provider: auth.provider, uid: auth.uid)
+        user_provider.save
+        user
+        # Link FB provider for that user who already exist on our system.
+      else
+        # "Create New User and it's provider"
+        # "User object and 1 provider for that user"
+        user = User.create(name: auth.info.name, email: auth.info.email, password: Devise.friendly_token[0,20])
+        user_provider = user.user_providers.create(provider:auth.provider, uid:auth.uid)
+        user
+      end
     end
-end
+
+    # unless user.nil?
+    #     user.user
+    # else
+    #     registered_user = User.where(:email => auth.info.email).first
+    #     unless registered_user.nil?
+    #                 UserProvider.create!(
+    #                       provider: auth.provider,
+    #                       uid: auth.uid,
+    #                       user_id: registered_user.id
+    #                       )
+    #         registered_user
+    #     else
+    #       debugger
+    #         user = User.create!(
+    #             name: auth.info.name,
+    #                     email: auth.info.email,
+    #                     password: Devise.friendly_token[0,20],
+    #                     )
+    #         user_provider = UserProvider.create!(
+    #             provider:auth.provider,
+    #                     uid:auth.uid,
+    #                       user_id: user.id
+    #             )
+    #         user
+    #     end
+    # end
+  end
+
+  def self.find_for_google_oauth(auth)
+
+    if user_provider.present?
+      user_provider.user
+      # "User login and redirect to landing page"
+    else
+      # New user signup
+      # Email: abc@gmail.com
+      # FB: abc@gmail.com
+      user = User.where(email: auth.info.email).first
+      # "fetch email from auth details and if Email ID already exist"
+      if user.present?
+        user_provider = user.user_providers.new(provider: auth.provider, uid: auth.uid)
+        user_provider.save
+        user
+        # Link FB provider for that user who already exist on our system.
+      else
+        # "Create New User and it's provider"
+        # "User object and 1 provider for that user"
+        user = User.create(name: auth.info.name, email: auth.info.email, password: Devise.friendly_token[0,20])
+        user_provider = user.user_providers.create(provider:auth.provider, uid:auth.uid)
+        user
+      end
+    end
+
+    # user = UserProvider.where(:provider => auth.provider, :uid => auth.uid).first
+    # unless user.nil?
+    #     user.user
+    # else
+    #     registered_user = User.where(:email => auth.info.email).first
+    #     unless registered_user.nil?
+    #                 UserProvider.create!(
+    #                       provider: auth.provider,
+    #                       uid: auth.uid,
+    #                       user_id: registered_user.id
+    #                       )
+    #         registered_user
+    #     else
+    #         user = User.create!(
+    #             name: auth.info.name,
+    #                       email: auth.info.email,
+    #                       password: Devise.friendly_token[0,20],
+    #                     )
+    #         user_provider = UserProvider.create!(
+    #             provider:auth.provider,
+    #                     uid:auth.uid,
+    #                     user_id: user.id
+    #             )
+    #         user
+    #     end
+    # end
+  end
 
 end
